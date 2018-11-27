f7204a6a94cbb84e947bf66a46211bfd5b0d80d9
knzn <rdacles08@gmail.com>
2018-11-27 09:49:00 +0800

    first

---
+++ boards/tests/test.views.py
@@ -0,0 +1,33 @@
+# from ..views import home, board_topics, new_topic
+# from ..models import Board, Topic, Post
+# from ..forms import NewTopicForm
+# from django.test import TestCase
+# from django.contrib.auth.models import User
+# from django.urls import resolve, reverse
+# # from .views import home, board_topics, new_topic
+# # from .models import Board, Topic, Post
+# # from .forms import NewTopicForm
+
+
+# class HomeTests(TestCase):
+#     def setUp(self):
+#         self.board = Board.objects.create(name='Django', description='Django board.')
+#         url = reverse('home')
+#         self.response = self.client.get(url)
+
+#     def test_home_view_status_code(self):
+#         self.assertEquals(self.response.status_code, 200)
+
+#     def test_home_url_resolves_home_view(self):
+#         view = resolve('/')
+#         self.assertEquals(view.func, home)
+
+#     def test_home_view_contains_link_to_topics_page(self):
+#         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+#         self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+#     def test_board_topics_view_contains_link_back_to_homepage(self):
+#         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+#         response = self.client.get(board_topics_url)
+#         homepage_url = reverse('home')
+#         self.assertContains(response, 'href="{0}"'.format(homepage_url))
---
+++ myproject/urls.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.urls import path
+from boards import views
+from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.BoardListView.as_view(), name='home'),
+    path('boards/<int:pk>/', views.TopicListView.as_view(), name='board_topics'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/topics/<topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<topic_pk>/posts/<post_pk>/edit', views.PostUpdateView.as_view(), name='edit_post'),
+    path('boards/<int:pk>/topics/<topic_pk>/reply/', views.reply_topic, name='reply_topic'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset/',
+        auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+        ),
+        name='password_reset'),
+    path('reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    path('reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+    path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+    name='password_change'),
+    path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+    name='password_change_done'),
+]
